79b8285b2413a42266a67b4c746ae1723f691872
Jess Sullivan <jess@sulliwood.org>
2026-02-12 22:47:38 -0500

    fix: add Giscus repo-id and category-id

--- site/src/lib/components/Giscus.svelte
+++ site/src/lib/components/Giscus.svelte
@@ -6,11 +6,9 @@
 		const script = document.createElement('script');
 		script.src = 'https://giscus.app/client.js';
 		script.setAttribute('data-repo', 'Jesssullivan/LA-Mesh');
-		// TODO: Fill these after enabling GitHub Discussions on the repo
-		// Get values from https://giscus.app after setup
-		script.setAttribute('data-repo-id', '');
-		script.setAttribute('data-category', 'Docs Comments');
-		script.setAttribute('data-category-id', '');
+		script.setAttribute('data-repo-id', 'R_kgDOROXdFQ');
+		script.setAttribute('data-category', 'General');
+		script.setAttribute('data-category-id', 'DIC_kwDOROXdFc4C2VRY');
 		script.setAttribute('data-mapping', 'pathname');
 		script.setAttribute('data-strict', '0');
 		script.setAttribute('data-reactions-enabled', '1');
